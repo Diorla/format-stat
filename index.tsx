5623fc2ec35a1fc3db37c720947243de7158908f
Diorla <adedotster@gmail.com>
2022-03-28 09:08:46 +0100

    add dropdown

--- index.tsx
+++ index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import PointBiserial from './PointBiserial';
 import './style.css';
 
 const App = () => {
+  const [type, setType] = useState('pb');
   return (
     <div>
-      <PointBiserial />
+      <select
+        name="stat type"
+        id="stats"
+        onChange={(e) => setType(e.target.value)}
+      >
+        <option value="pb">Point Biserial</option>
+        <option value="ch">Chi-square</option>
+      </select>
+      {type === 'pb' && <PointBiserial />}
     </div>
   );
 };
